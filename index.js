fa9302af21a3e043b19b43669dd4f031f2236692
Boof <97455552+hexahigh@users.noreply.github.com>
2023-01-23 10:04:10 +0100

    test

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ const year = new Date().getFullYear();
 
 document.querySelector('.current-year').innerText = year;
 
+var userip = 0
 
 //Get user ip to put it in the contact form
 //Also check if they are using a vpn
 $.getJSON("https://europe-central2-portfolio-website-374313.cloudfunctions.net/getip", function(data) {
-    var userip = (data.ip);
+    userip = (data.ip);
     document.getElementById("formip").value = (data.ip);
     fetch('vpnip.txt')
     .then(response => response.arrayBuffer())
@@ -47,3 +48,18 @@ $.getJSON("https://europe-central2-portfolio-website-374313.cloudfunctions.net/g
     })
     .catch(error => console.error(error));
  })
+
+window.onload = function blockform() {
+    fetch('https://hexahigh.github.io/cdn/text/blockedformip.txt')
+    .then(response => response.arrayBuffer())
+    .then(arrayBuffer => {
+        const decoder = new TextDecoder('utf-8');
+        const data = decoder.decode(new Uint8Array(arrayBuffer));
+        const array = data.split('\n');
+        //console.log(array);
+        var isblocked = false;
+        if (array.includes(userip)) {
+            
+        }
+    })
+}
