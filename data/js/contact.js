b12e7b4145654f04afe50ffb1efdb3ed7b9770c6
Boof <97455552+hexahigh@users.noreply.github.com>
2023-02-06 13:45:12 +0100

    YAY

---
+++ data/js/contact.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#formid')
+    const addButton = document.querySelector('#submitbutton')
+    //const updateButton = document.querySelector('#updateAddress')
+  
+    addButton && addButton.addEventListener('click', (e) => {
+      e.preventDefault()
+  
+      if (!form.checkValidity()) {
+        return
+      }
+  
+      fetch('https://22242122.sibforms.com/serve/MUIEAFviVKMOgXKqIKahcIUcPU4E2qrOD5RoywBglC-uPPsZ1pivAjzG8KDv2COk2wTywJIcOMSA582duFkm5WlVF-LXT-CeloWSFr1zbi2uTI2kSd7HEO0QHHeYoD5AaUdCK1z5msZU8QGtV4UvacSmww2AM1sYE94g9IDNjjehiMdizx82c4WuXJAeN3LJH81P4lFZK1Qms5bI', { method: 'POST', body: new FormData(form) })
+        .then((r => r.json()))
+        .then(console.log)
+    })
+    
+    /*updateButton && updateButton.addEventListener('click', (e) => {
+      e.preventDefault()
+  
+      if (!form.checkValidity()) {
+        return
+      }
+  
+      fetch('/updateAddress', { method: 'POST', body: new FormData(form) })
+        .then((r => r.json()))
+        .then(console.log)
+    })*/
+  })
