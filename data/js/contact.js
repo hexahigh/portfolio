3879b934e57dc930fa8917adfa998ec7feee71fa
Boof <97455552+hexahigh@users.noreply.github.com>
2023-02-06 13:55:12 +0100

    test

--- data/js/contact.js
+++ data/js/contact.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
       fetch('https://22242122.sibforms.com/serve/MUIEAFviVKMOgXKqIKahcIUcPU4E2qrOD5RoywBglC-uPPsZ1pivAjzG8KDv2COk2wTywJIcOMSA582duFkm5WlVF-LXT-CeloWSFr1zbi2uTI2kSd7HEO0QHHeYoD5AaUdCK1z5msZU8QGtV4UvacSmww2AM1sYE94g9IDNjjehiMdizx82c4WuXJAeN3LJH81P4lFZK1Qms5bI', { method: 'POST', body: new FormData(form) })
         .then((r => r.json()))
         .then(console.log)
+        console.log("I know there is an error, just ignore it.")
     })
     
     /*updateButton && updateButton.addEventListener('click', (e) => {
