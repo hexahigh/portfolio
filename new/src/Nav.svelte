2dfd9c51f75f01b006a488fbc80561ebecf0ac5c
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-08-05 23:32:42 +0200

    i need to watch more tutorials

--- new/src/Nav.svelte
+++ new/src/Nav.svelte
@@ -1,5 +1,20 @@
+<script>
+    import page from "page";
+    import Home from "./App.svelte";
+    import About from "./pages/About.svelte";
+
+    // set default component
+    let current = Home;
+    // Map routes to page. If a route is hit the current
+    // reference is set to the route's component
+    page("/", () => (current = Home));
+    page("/about", () => (current = About));
+    // activate router
+    page.start();
+</script>
+
 <nav>
-    <img src="data/images/logoround.png" alt="Boofdev logo">
+    <img src="data/images/logoround.png" alt="Boofdev logo" />
     <p class="text" style="font-family: Broadway-custom;">Boofdev</p>
     <ul id="nav-pc">
         <li><a href="index.html" id="nav-active">Home</a></li>
@@ -10,8 +25,15 @@
         <li><a target="_blank" id="nav-donate">Donate</a></li>
     </ul>
     <ul id="nav-mobile">
-        <li id="nav-menu-li"><button id="nav-menu"><img src="data/images/menu.svg" alt="menu"
-                    id="nav-menu-img"></button></li>
+        <li id="nav-menu-li">
+            <button id="nav-menu"
+                ><img
+                    src="data/images/menu.svg"
+                    alt="menu"
+                    id="nav-menu-img"
+                /></button
+            >
+        </li>
     </ul>
 </nav>
 
@@ -22,6 +44,12 @@
         <li><a href="news/">News</a></li>
         <li><a href="projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a target="_blank" href="https://patreon.com/boofdev" id="nav-donate-mobile">Donate</a></li>
+        <li>
+            <a
+                target="_blank"
+                href="https://patreon.com/boofdev"
+                id="nav-donate-mobile">Donate</a
+            >
+        </li>
     </ul>
 </div>
