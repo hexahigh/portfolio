f5b3fad8ae0fb4f17e73f3b7b00f6e1d9537dd82
Boof <97455552+hexahigh@users.noreply.github.com>
2023-08-29 11:44:07 +0000

    fix?

--- src/routes/api/analytics/+server.js
+++ src/routes/api/analytics/+server.js
@@ -2,11 +2,11 @@ import PocketBase from pocketbase
 
 const pb = new PocketBase('https://db.080609.xyz');
 
-export async function GET({ url }) {
+export function GET({ url }) {
     const ip = url.searchParams.get('ip');
     const date = url.searchParams.get('date');
 
-    const record = await pb.collection('homepage_analytics').create({
+    pb.collection('homepage_analytics').create({
         ip: ip,
         time: date,
     });
