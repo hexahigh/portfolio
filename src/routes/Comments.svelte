b0298bca8e885b7dbeb0ee5c31dc1bfdbf3de0f6
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-01 09:33:39 +0200

    work?

--- src/routes/Comments.svelte
+++ src/routes/Comments.svelte
@@ -1,6 +1,7 @@
 <script>
+    import { onMount } from "svelte";
     import PocketBase from "pocketbase";
-    import { FullList } from 'svelte-pocketbase';
+    import { FullList } from "svelte-pocketbase";
     const pb = new PocketBase("https://db.080609.xyz");
 
     async function addComment(user, comment) {
@@ -15,15 +16,16 @@
             console.error("Failed to add comment:", error);
         }
     }
-
-    document
-        .getElementById("comment-form")
-        .addEventListener("submit", function (event) {
-            event.preventDefault();
-            var user = document.getElementById("user").value;
-            var comment = document.getElementById("comment").value;
-            addComment(user, comment);
-        });
+    onMount(() => {
+        document
+            .getElementById("comment-form")
+            .addEventListener("submit", function (event) {
+                event.preventDefault();
+                var user = document.getElementById("user").value;
+                var comment = document.getElementById("comment").value;
+                addComment(user, comment);
+            });
+    });
 </script>
 
 <form id="comment-form">
@@ -33,8 +35,8 @@
 </form>
 <FullList collection="comments" let:records>
     {#each records as record}
-      <p>{record.user}</p>
-      <p>{record.comment}</p>
+        <p>{record.user}</p>
+        <p>{record.comment}</p>
     {/each}
     <span slot="error" let:error>{error}</span>
-  </FullList>
+</FullList>
