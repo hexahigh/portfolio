f41bfa297cfed0ac758e40ba982ece6fdf9ee713
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-01 10:14:27 +0200

    added some css to make it nice

--- src/routes/Comments.svelte
+++ src/routes/Comments.svelte
@@ -1,100 +1,27 @@
-<style>
-    :root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color-scheme: light dark;
-  color: rgba(255, 255, 255, 0.87);
-  background-color: #242424;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-a:hover {
-  color: #535bf2;
-}
-
-body {
-  margin: 0;
-  display: flex;
-  place-items: center;
-  min-width: 320px;
-  min-height: 100vh;
-}
-
-h1 {
-  font-size: 3.2em;
-  line-height: 1.1;
-}
-
-.card {
-  padding: 2em;
-}
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  text-align: center;
-}
-
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #1a1a1a;
-  cursor: pointer;
-  transition: border-color 0.25s;
-}
-button:hover {
-  border-color: #646cff;
-}
-button:focus,
-button:focus-visible {
-  outline: 4px auto -webkit-focus-ring-color;
-}
-
-@media (prefers-color-scheme: light) {
-  :root {
-    color: #213547;
-    background-color: #ffffff;
-  }
-  a:hover {
-    color: #747bff;
-  }
-  button {
-    background-color: #f9f9f9;
-  }
-}
-</style>
-
 <script>
     import { onMount } from "svelte";
+    import axios from "axios";
     import PocketBase from "pocketbase";
-    import { pbStore, FullList } from 'svelte-pocketbase';
+    import { pbStore, FullList } from "svelte-pocketbase";
     const pb = new PocketBase("https://db.080609.xyz");
     pbStore.set("https://db.080609.xyz");
 
     async function addComment(user, comment) {
         try {
+            // Generate the avatar URL using the DiceBear API
+            const avatarUrl = `https://avatars.dicebear.com/api/avataaars/${user}.svg`;
+
+            // Make a request to retrieve the avatar image
+            const response = await axios.get(avatarUrl, {
+                responseType: "text",
+            });
+
+            // Create a new comment object with the user's avatar
             const newComment = await pb.collection("comments").create({
                 user,
                 comment,
                 timestamp: new Date().toISOString(), // store the current date and time as the timestamp
+                avatar: response.data, // Store the SVG avatar image
             });
             console.log("Comment added:", newComment);
         } catch (error) {
@@ -110,7 +37,6 @@ button:focus-visible {
                 var comment = document.getElementById("comment").value;
                 addComment(user, comment);
             });
-
     });
 </script>
 
@@ -119,15 +45,51 @@ button:focus-visible {
     <textarea id="comment" placeholder="Your comment" />
     <button type="submit">Submit</button>
 </form>
-<div id="comments-display"></div>
+<div id="comments-display" />
 
 <FullList collection="comments" batch={50} let:records>
     {#each records as record}
-      <div>
-        <p>{record.user}</p>
-        <p>{record.timestamp}</p>
-        <p>{record.comment}</p>
-      </div>
+        <div class="comments-display">
+            <img src={record.avatar} alt="User Avatar" width="50" height="50" />
+            <div class="right-side">
+                <div class="commentHeader">
+                    <p>{record.user}</p>
+                    <p>{record.timestamp}</p>
+                </div>
+                <div class="commentText">
+                    <p>{record.comment}</p>
+                </div>
+            </div>
+        </div>
     {/each}
     <span slot="error" let:error>{error}</span>
-  </FullList>
+</FullList>
+
+<style>
+    .comments-display {
+        background-color: #1b1b1b;
+        min-width: 250px;
+    }
+
+    .right-side {
+        float: left;
+        width: calc(100% - 100px);
+    }
+
+    .commentHeader {
+        text-align: left;
+        display: block;
+        padding: 10px;
+        margin-top: 2px;
+        display: inline-block;
+    }
+
+    .commentText {
+        text-align: left;
+        padding-left: 10px;
+        color: #978f8f;
+        word-wrap: break-word;
+        min-height: 30px;
+        padding-bottom: 10px;
+    }
+</style>
