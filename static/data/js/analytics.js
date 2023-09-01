7d5e3e0f88363a987c3a8bb59de33cbe849bc99c
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-01 14:31:01 +0200

    now it should work

--- static/data/js/analytics.js
+++ static/data/js/analytics.js
@@ -1,12 +1,18 @@
-let ip = ""
+let ip = "";
 
 $.getJSON("https://europe-central2-portfolio-website-374313.cloudfunctions.net/getip", function(data) {
-    ip = data.ip
+    ip = data.ip;
+    const ua = window.navigator.userAgent;
+    const page = window.location;
+    const url = "https://boofdev.eu/api/analytics?ip=" + ip + "&ua=" + ua + "&url=" + page;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('There has been a problem with your fetch operation:', error));
 });
-const ua = window.navigator.userAgent
-const page = window.location
-
-
-const url = "https://boofdev.eu/api/analytics?ip=" + ip + "&ua=" + ua + "&url=" + page
-
-fetch(url);
