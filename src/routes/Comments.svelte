50695717f4b3e247f04dd1ec0e05010699fee6fa
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-05 14:50:58 +0200

    added a switch to turn hidden comments on and off

--- src/routes/Comments.svelte
+++ src/routes/Comments.svelte
@@ -11,6 +11,9 @@
     let databaseError = false;
     let comments = [];
     let badWords = [];
+    let showBlocked = false;
+
+    $: fetchComments(), showBlocked;
 
     async function fetchComments() {
         try {
@@ -18,7 +21,7 @@
             comments = comments.filter(
                 (comment) =>
                     comment.PID === window.location.pathname &&
-                    comment.blocked !== true
+                    (showBlocked || !comment.blocked)
             );
         } catch (error) {
             console.error("Failed to fetch comments:", error);
@@ -86,7 +89,10 @@
     <button type="submit">Submit</button>
 </form>
 <div id="comments-display" />
-
+<div class="switch">
+    <input type="checkbox" id="switch" bind:checked={showBlocked} />
+    <label for="switch">Show blocked comments</label>
+</div>  
 <div class="comments-display-wrapper">
     {#each comments as record}
         <div class="comments-display">
@@ -129,6 +135,54 @@
 </div>
 
 <style>
+    /* The switch - the box around the slider */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    /* The slider */
+    .switch label {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        transition: 0.4s;
+        border-radius: 34px;
+    }
+
+    .switch label:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        transition: 0.4s;
+        border-radius: 50%;
+    }
+
+    input:checked + label {
+        background-color: #1470e9;
+    }
+
+    input:checked + label:before {
+        transform: translateX(26px);
+    }
+
     .error-notice {
         text-align: center;
         color: #f5a803;
