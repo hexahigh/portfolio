9b886ec77ff83b19056058cbe5c09de9947b9f52
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-06 12:29:37 +0000

    now?

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,25 @@
 <script>
-    import { page } from '$app/stores';
+    import { page } from "$app/stores";
 </script>
 
-<img src="https://http.cat/{$page.status}" alt="{$page.status}" style="height: 100%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+<body>
+    <img src="https://http.cat/{$page.status}" alt={$page.status} class="bg" />
+</body>
+
+<style>
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+    }
+
+    .bg {
+        /* Full height */
+        height: 100%;
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
