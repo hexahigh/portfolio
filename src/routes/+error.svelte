b18e35ee995880be1e994c23b9bcbeb0dfff655d
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-07 08:07:15 +0000

    work

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -13,8 +13,8 @@
         left: 0;
         right: 0;
         bottom: 0;
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         object-fit: cover;
     }
 </style>
