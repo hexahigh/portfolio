e150f28ef60aa34951375073da311277638f035e
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-07 08:36:02 +0000

    do i even need the css?

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,19 +2,4 @@
     import { page } from "$app/stores";
 </script>
 
-    <img src="https://http.cat/{$page.status}.jpg" alt={$page.status}/>
-
-<style>
-    img {
-        margin-top: 10%;
-        margin-bottom: 10%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-</style>
+<img src="https://http.cat/{$page.status}.jpg" alt={$page.status} />
