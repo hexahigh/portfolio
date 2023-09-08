fa45d93fee1d77ac21a297b7e520187826517b6c
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-08 12:04:13 +0000

    added an api that returns a png image instead of an svg

---
+++ src/routes/api/avatar/png/+server.js
@@ -0,0 +1,34 @@
+import { createAvatar } from '@dicebear/core';
+import { identicon } from '@dicebear/collection';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+    try {
+        const avatar = createAvatar(identicon, {
+            seed: makeid(30),
+        });
+
+        const data = avatar.png(); 
+        
+        return new Response(data, { 
+            status: 200,
+            headers: { 'Content-Type': 'image/png' }
+        });
+        
+    } catch (error) {
+        console.error(error);
+        return new Response("An error occurred: ", error, { status: 500 });
+    }
+}
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
