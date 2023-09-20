7d769b385bc5d86761e189426fa3a00218bb1f90
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-20 18:48:13 +0200

    Revert "now the torrent api is epic"
    This reverts commit db8ef689d035c026381dd4fb32679dd3d297d202.

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,10 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	preprocess: {
-		// ...
-		vite: {
-		   resolve: {
-			  alias: {
-				fs: path.resolve('./node_modules/browserfs/dist/shims/fs.js'),
-				path: path.resolve('./node_modules/path-browserify'),
-				assert: path.resolve('./node_modules/assert'),
-				util: path.resolve('./node_modules/util'),
-			  },
-		   },
-		},
-	 },
+	}
 };
 
 export default config;
