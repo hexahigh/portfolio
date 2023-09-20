316b23b47a838b80629d86ad8bca34a5fe42171c
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-20 18:36:23 +0200

    fixed build?

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,20 @@ import adapter from '@sveltejs/adapter-cloudflare';
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: {
+		// ...
+		vite: {
+		   resolve: {
+			  alias: {
+				fs: path.resolve('./node_modules/browserfs/dist/shims/fs.js'),
+				path: path.resolve('./node_modules/path-browserify'),
+				assert: path.resolve('./node_modules/assert'),
+				util: path.resolve('./node_modules/util'),
+			  },
+		   },
+		},
+	 },
 };
 
 export default config;
