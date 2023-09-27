aaf68d8d60680afe88761080f0a75a5ab79f1cf8
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-27 12:12:38 +0000

    updated analytics

--- src/routes/api/analytics2/+server.js
+++ src/routes/api/analytics2/+server.js
@@ -7,12 +7,18 @@ export async function GET({ request }) {
     const ip = request.headers.get('x-forwarded-for') || request.headers.get('remote_addr');
     const page = request.headers.get('referer');
     const ua = request.headers.get('user-agent');
+    const sec_platform = request.headers.get('Sec-Ch-Ua-Platform')
+    const sec_gpc = request.headers.get('Sec-Gpc')
+    const dnt = request.headers.get('Dnt')
 
     try {
         await pb.collection('analytics_2').create({
             ip: ip,
             url: page,
             ua: ua,
+            sec_platform: sec_platform,
+            sec_gpc: sec_gpc,
+            dnt: dnt,
         });
         return new Response("sent", {status: 200});
     } catch (error) {
