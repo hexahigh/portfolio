2cb023fb522ea72a82d7d909314801a8349ce4eb
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-09-27 16:28:42 +0200

    no more tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
