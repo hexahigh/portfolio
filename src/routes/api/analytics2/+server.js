4210cc893d2e08b34885de780bd55012fefcef0a
Boof <97455552+hexahigh@users.noreply.github.com>
2023-09-27 12:34:56 +0000

    maybe fixed analytics

--- src/routes/api/analytics2/+server.js
+++ src/routes/api/analytics2/+server.js
@@ -11,7 +11,7 @@ export async function GET({ request }) {
     let sec_gpc = request.headers.get('Sec-Gpc')
     let dnt = request.headers.get('Dnt')
 
-    if(dnt != 0 || sec_gpc != 0) {
+    if(dnt == "1" || sec_gpc == "1") {
         ip = "'DNT'"
     };
 
