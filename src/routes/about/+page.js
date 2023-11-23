ea39e2bd291aa40efb49bd7d13a98986f9ceccff
Boofdev <hexahigh0@gmail.com>
2023-11-23 12:30:08 +0100

    vscode no workie

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,5 +1,5 @@
-/** @type {import('./$types').PageLoad} */ export function load({ params }) {
-    let fact0 = fetch("https://boofdev.eu/api/fact")
+/** @type {import('./$types').PageLoad} */ export async function load({ params }) {
+    let fact0 = await fetch("https://boofdev.eu/api/fact")
     let fact1 = fact0.text()
   return {
     fact: fact1
